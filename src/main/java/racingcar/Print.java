13a1b4fa0962feb3bfadb76b46ff78f523e5f69d
soulfever <soulfever01@naver.com>
2021-12-16 16:00:33 +0900

    FEAT : ArrayList<Car> 생성하는 기능

--- src/main/java/racingcar/Print.java
+++ src/main/java/racingcar/Print.java
@@ -5,21 +5,34 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class Print {
-    public static ArrayList<Car> carArrayList = new ArrayList<>();
     static final int MAX_NAME_LENGTH = 5;
+
+    public static ArrayList<Car> carArrayList = new ArrayList<>();
+    private static String[] carsName;
 
     public static void InputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[]carsName = Console.readLine().split(",");
-        CheckCarName(carsName);
+        carsName = Console.readLine().split(",");
+        CheckCarName();
     }
 
-    private static void CheckCarName(String[] carsName) {
-        for (int i = 0; i < carsName.length; i++) {
-            if (carsName[i].length() > MAX_NAME_LENGTH) {
+    private static void CheckCarName() {
+        if (carsName.length == 0) {
+            Except.NotValidName();
+            return;
+        }
+        for (String name : carsName) {
+            if (name.length() > MAX_NAME_LENGTH) {
                 Except.NotValidName();
                 return;
             }
+        }
+        MakeCarArrayList();
+    }
+
+    private static void MakeCarArrayList() {
+        for (String name : carsName) {
+            carArrayList.add(new Car(name));
         }
     }
 }
